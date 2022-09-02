0da21423278af8c4524d35d0fd5673e4a6408cbd
Paula Ramírez <ramirezpaulaandre148@gmail.com>
2022-09-01 20:10:51 -0500

    avanzando

--- PI-Pokemon-main/api/src/routes/pokemon.js
+++ PI-Pokemon-main/api/src/routes/pokemon.js
@@ -1,40 +1,103 @@
-const { Router } = require('express');
-const { Pokemon } = require('../db');
+const { Router, response } = require('express');
+const { Pokemon, Tipo} = require('../db');
 const router = Router();
+const axios = require('axios');
+
 
 router.get('/', (req, res, next) => {
-    try {
-      let pokemones = Pokemon.findAll()
-      res.status(201).json(pokemones);
-    } catch (error) {
-      res.status(404).json({ error });
+  const name = req.query.name;
+  let pokemonApi
+  let pokemonDb
+  try {
+    pokemonApi = axios.get('https://pokeapi.co/api/v2/pokemon');
+    pokemonDb = Pokemon.findAll({
+        includes: Tipo
+    })
+    Promise.all([
+      pokemonApi,
+      pokemonDb
+    ])
+      .then((respuesta) => {
+        const [pokemonApi, pokemonDb] = respuesta;
+        let filterPokemon = pokemonApi.data.results 
+        
+        filterPokemon = filterPokemon.map((pokemon) => {
+          return {
+            name: pokemon.name,
+          }   
+        })
+        let allPokemons = [...filterPokemon, ...pokemonDb];
+        res.send(allPokemons);
+    })
+    
+  } catch (error) {
+    next(error);
+  }
+});
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id
+    let pokemons
+    if (typeof id === 'string' && id.length > 8) {
+      pokemons = await Pokemon.findByPk(id)
+      res.send(pokemons);
+    } else {
+      respuesta = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      pokemons = respuesta.data
+      let pokeArray = []
+      pokeArray.push(pokemons);
+      pokeArray = pokeArray.map(pokemon => {
+        return {
+          imagen: pokemon.sprites.front_default,
+          name: pokemon.name,
+          type: pokemon.types['0'].type.name,
+          //type2: pokemon.types['1'].type.name,
+          id: pokemon.id,
+          vida: pokemon.base_experience,
+          ataque: pokemon.stats['0'].base_stat,
+          altura: pokemon.height,
+          peso: pokemon.weight
+        }
+      })
+      res.send(pokeArray);
     }
+    res.send(pokemons);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get(`/:id`, (req, res, next) => {
-  const { id } = req.params;
-  let findPokemon = Pokemon.findByPk(id);
-
-  if (!findPokemon) return res.status(404).send(`El Pokemon Id ${id} no existe`);
-
-  return res.json(findPokemon);
-});
-
-router.get('/', (req, res, next) => {
-  const { name } = req.query;
-  const pokemon = Pokemon.findByPk(name);
-
-  if (!pokemon) return res.status(404).send(`El nombre ${name} no corresponde a un Pokemon`);
-
-  return res.json(pokemon);
-
-});
+/* router.get(`/`, (req, res, next) => {
+  const name = req.query.name
+  try {
+    if (name) {
+      //let pokemonApi = axios.get(`https://pokeapi.co/api/v2/pokemon?name=${name}`);
+      let pokemonDb = Pokemon.findAll({
+        includes: Tipo,
+        where: {
+          name: name
+        }
+      });
+      res.send(pokemonDb);
+    } else {
+      res.send(`El nombre ${name}, no pertenece a un Pokemon existente`);
+    }
+    
+  } catch (error) {
+        next(error);
+  }
+}); */
 
 router.post('/', async (req, res, next) => {
-  const { id, name, vida, ataque, defensa, velocidad, altura, peso } = req.body;
-  const newPokemon = await Pokemon.create(req.body);
-  res.json(newPokemon);
+  try {
+    const { id, name, vida, ataque, defensa, velocidad, altura, peso } = req.body;
+    const newPokemon = await Pokemon.create(req.body);
+    res.json(newPokemon); 
+  } catch (error) {
+        next(error);
+  }
 });
+
 
 
 module.exports = router;
--- PI-Pokemon-main/api/src/routes/tipo.js
+++ PI-Pokemon-main/api/src/routes/tipo.js
@@ -1,9 +1,31 @@
 const { Router } = require('express');
 const { Tipo } = require('../db');
 const router = Router();
+const axios = require('axios');
 
 router.get('/', (req, res, next) => {
-  console.log('Hola soy post en tipo');
+  try {
+    let pokemonApi = axios.get('https://pokeapi.co/api/v2/type/');
+    let pokemonDb = Tipo.findAll()
+    Promise.all([
+      pokemonApi,
+      pokemonDb
+    ])
+      .then((respuesta) => {
+        const [pokemonApi, pokemonDb] = respuesta;
+        let filterPokemon = pokemonApi.data.results
+        filterPokemon = filterPokemon.map((type) => {
+          return {
+            name: type.name,
+          }   
+        })
+        let allPokemons = [...filterPokemon, ...pokemonDb];
+        res.send(allPokemons);
+    })
+    
+  } catch (error) {
+        next(error);
+  }
 })
 
 module.exports = router;
